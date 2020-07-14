2763e76fc793f4744cc787defedeebe2a4ef1add
ErikZambeli <erik_12_nf@live.com>
2020-07-14 20:20:59 -0300

    feat: form login

--- e-book-mobile/src/pages/Login/index.tsx
+++ e-book-mobile/src/pages/Login/index.tsx
@@ -2,67 +2,184 @@ import React, { useState } from "react";
 import {
   View,
   Text,
-  TextInput,
   NativeSyntheticEvent,
   TextInputChangeEventData,
 } from "react-native";
 import { Feather as Icon } from "@expo/vector-icons";
 import { Input } from "react-native-elements";
+import { RectButton } from "react-native-gesture-handler";
+import { withFormik, FormikProps } from "formik";
+import * as Yup from "yup";
 
 import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 interface InitialValues {
   email: string;
   password: string;
+  dispatch: (values: any) => void;
 }
 
-const Login: React.FC = () => {
-  const [initialValues, setInitialValues] = useState<InitialValues>({
-    email: "",
-    password: "",
-  });
+interface MyFormProps {
+  initialEmail: string;
+  initialPassword: string;
+  dispatch: (values: any) => void;
+}
 
-  function handleChangeEmail(
-    email: NativeSyntheticEvent<TextInputChangeEventData>
-  ) {
-    setInitialValues({
-      email: email.nativeEvent.text,
-      password: initialValues.password,
-    });
+const Form = (props: FormikProps<InitialValues>) => {
+  const navigation = useNavigation();
+  const [visible, setVisible] = useState(true);
+
+  const {
+    values,
+    errors,
+    touched,
+    setFieldValue,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+  } = props;
+
+  //mudar aqui para o reducer
+  props.values.dispatch = (consloe) => {
+    console.log(consloe);
+  };
+
+  function handleChangeVisible() {
+    if (visible) setVisible(false);
+    else setVisible(true);
   }
 
-  function handleChangePassword(
-    password: NativeSyntheticEvent<TextInputChangeEventData>
+  function handleChangeEmail(
+    value: NativeSyntheticEvent<TextInputChangeEventData>
   ) {
-    setInitialValues({
-      email: initialValues.email,
-      password: password.nativeEvent.text,
-    });
+    setFieldValue("email", value.nativeEvent.text);
+  }
+  function handleChangePassword(
+    value: NativeSyntheticEvent<TextInputChangeEventData>
+  ) {
+    setFieldValue("password", value.nativeEvent.text);
+  }
+
+  function handleNavigationToCreateAccount() {
+    navigation.navigate("CreateAccount");
+  }
+
+  function handleSubmitForm() {
+    handleSubmit();
   }
 
   return (
     <View style={styles.container}>
-      <Text>Let’s Sign You In</Text>
-      <Text>Welcome back, you’ve been missed!</Text>
+      <Text style={styles.textTitle}>Let’s Sign You In</Text>
+      <Text style={styles.textSubTitle}>Welcome back, you’ve been missed!</Text>
 
-      <View>
-        <Text style={styles.textEmail}>Email</Text>
+      <View style={styles.main}>
         <Input
+          nativeID="email"
+          value={values.email}
+          onBlur={handleBlur("email")}
+          label={<Text style={styles.textEmail}>Email</Text>}
+          inputStyle={styles.textInputEmail}
           placeholder="Type here your email"
           leftIcon={<Icon name="user" size={24} color="black" />}
-          onChange={handleChangeEmail}
+          onChange={(value) => setFieldValue("email", value.nativeEvent.text)}
+          errorStyle={{ color: "red" }}
+          errorMessage={
+            errors.email && touched.email && errors.email
+              ? `${errors.email}`
+              : ""
+          }
         />
-        <Text style={styles.textPassword}>Password</Text>
         <Input
+          nativeID="password"
+          value={values.password}
+          onBlur={handleBlur("password")}
+          label={<Text style={styles.textPassword}>Password</Text>}
+          inputStyle={styles.textInputPassword}
           placeholder="Type here your password"
           leftIcon={<Icon name="key" size={24} color="black" />}
+          secureTextEntry={visible}
+          rightIcon={
+            visible ? (
+              <Icon
+                onPress={handleChangeVisible}
+                name="eye-off"
+                size={24}
+                color="black"
+              />
+            ) : (
+              <Icon
+                onPress={handleChangeVisible}
+                name="eye"
+                size={24}
+                color="black"
+              />
+            )
+          }
           onChange={handleChangePassword}
-          // errorStyle={{ color: "red" }}
-          // errorMessage="ENTER A VALID ERROR HERE"
+          errorStyle={{ color: "red" }}
+          errorMessage={
+            errors.password && touched.password && errors.password
+              ? `${errors.password}`
+              : ""
+          }
         />
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleSubmitForm}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="log-in" color="#fff" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </RectButton>
+      </View>
+
+      <View style={styles.mainFooter}>
+        <Text style={styles.footerText}>Don't have an account?</Text>
+        <RectButton
+          style={styles.buttonLink}
+          onPress={handleNavigationToCreateAccount}
+        >
+          <Text style={styles.buttonTextCreate}>Sign up</Text>
+        </RectButton>
       </View>
     </View>
   );
 };
+
+const Login = withFormik<MyFormProps, InitialValues>({
+  mapPropsToValues: (props) => ({
+    email: props.initialEmail || "",
+    password: props.initialPassword || "",
+    dispatch: props.dispatch,
+  }),
+
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email("Email not valid").required("Email is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(5, "Min 5 character"),
+  }),
+
+  handleSubmit(
+    { email, password, dispatch }: InitialValues,
+    { props, setSubmitting, setErrors }
+  ) {
+    const user = {
+      email: email,
+      password: password,
+    };
+    console.log(user);
+    try {
+      dispatch(user);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+})(Form);
 
 export default Login;
--- e-book-mobile/src/pages/Login/styles.ts
+++ e-book-mobile/src/pages/Login/styles.ts
@@ -3,34 +3,115 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffe19c",
+    backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center",
+    padding: 16,
   },
+
+  main: {
+    marginTop: 48,
+  },
+
   textEmail: {
     fontSize: 16,
+    paddingTop: 8,
+    paddingLeft: 8,
     fontFamily: "Roboto_500Medium",
   },
   textPassword: {
+    fontSize: 16,
+    paddingTop: 8,
+    paddingLeft: 8,
+    fontFamily: "Roboto_500Medium",
+  },
+  textTitle: {
+    textAlign: "left",
+    padding: 8,
+    fontSize: 32,
+    fontFamily: "Ubuntu_700Bold",
+  },
+
+  textSubTitle: {
+    padding: 8,
     fontSize: 16,
     fontFamily: "Roboto_500Medium",
   },
   textInputEmail: {
     padding: 8,
-    height: 50,
-    width: 180,
     fontSize: 16,
-    borderBottomWidth: 2,
-    borderBottomColor: "#000",
-    borderRadius: 20,
     backgroundColor: "#fff",
   },
   textInputPassword: {
     padding: 8,
-    height: 50,
-    width: 180,
-    borderRadius: 20,
+    fontSize: 16,
     backgroundColor: "#fff",
+  },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#D2DFFF",
+    height: 50,
+    borderRadius: 24,
+    overflow: "hidden",
+  },
+
+  buttonIcon: {
+    height: 50,
+    width: 60,
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    borderRadius: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: "center",
+    textAlign: "center",
+    color: "#4E67A8",
+    fontFamily: "Roboto_500Medium",
+    fontSize: 16,
+    paddingRight: 40,
+  },
+  buttonLink: {
+    backgroundColor: "transparent",
+    height: 40,
+    width: 80,
+    padding: 8,
+    flexDirection: "row",
+    borderRadius: 32,
+    overflow: "hidden",
+    alignItems: "center",
+  },
+  footerText: {
+    flex: 1,
+    justifyContent: "center",
+    textAlign: "right",
+    color: "#000",
+    fontFamily: "Roboto_500Medium",
+    fontSize: 16,
+  },
+  footer: {
+    marginTop: 32,
+    padding: 8,
+  },
+
+  buttonTextCreate: {
+    flex: 1,
+    justifyContent: "center",
+    textAlign: "left",
+    textDecorationStyle: "solid",
+    textDecorationLine: "underline",
+    color: "#4E67A8",
+    fontFamily: "Roboto_500Medium",
+    fontSize: 16,
+  },
+  mainFooter: {
+    padding: 32,
+    marginRight: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
 
