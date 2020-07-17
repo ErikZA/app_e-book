e99fd20f9b6058221129a635a1c1c640b7b506e8
ErikZambeli <erik_12_nf@live.com>
2020-07-17 15:28:55 -0300

    feat:conect with backand

--- e-books-back/app/Controllers/Http/AuthController.ts
+++ e-books-back/app/Controllers/Http/AuthController.ts
@@ -1,11 +1,11 @@
-import Event from '@ioc:Adonis/Core/Event'
-import { DateTime } from 'luxon'
-import User from 'App/Models/User'
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { randomBytes } from 'crypto'
+import Event from "@ioc:Adonis/Core/Event";
+import { DateTime } from "luxon";
+import User from "App/Models/User";
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { randomBytes } from "crypto";
 
-import AuthValidator from 'App/Validators/AuthValidator'
-import UseToken from 'App/Models/UseToken'
+import AuthValidator from "App/Validators/AuthValidator";
+import UseToken from "App/Models/UseToken";
 
 export interface EventsList {
   sendMailForgotPassWord: () => {};
@@ -17,79 +17,79 @@ export default class AuthController {
    *
    * User Authentication
    */
-  public async auth ({ request, auth, response }: HttpContextContract) {
-    const data = await request.validate(AuthValidator)
+  public async auth({ request, auth, response }: HttpContextContract) {
+    const data = await request.validate(AuthValidator);
 
-    const token = await auth.attempt(data.email, data.password)
+    const token = await auth.attempt(data.email, data.password);
 
     return response.ok({
       ...token,
       ...data,
-    })
+    });
   }
 
   /**
    * Forgot Password
    */
-  public async forgotPassword ({ request, response }: HttpContextContract) {
-    const data = await request.only(['email'])
-    const token = randomBytes(20).toString('hex')
-    const now = new Date()
-    now.setHours(now.getHours() + 1)
+  public async forgotPassword({ request, response }: HttpContextContract) {
+    const data = request.only(["email"]);
+    const token = randomBytes(20).toString("hex");
+    const now = new Date();
+    now.setHours(now.getHours() + 1);
     if (data.email) {
       try {
-        const user = await User.findByOrFail('email', data.email)
+        const user = await User.findByOrFail("email", data.email);
         const useToken = await UseToken.create({
           user_id: user.id,
           passwordResetToken: token,
           passwordResetExpires: DateTime.fromJSDate(now),
-        })
+        });
 
         if (useToken) {
-          Event.emit('auth:send_mail_forgot_passWord', {
+          Event.emit("auth:send_mail_forgot_passWord", {
             email: user.email,
             token: token,
-          })
+          });
         }
         return response.ok({
           code: 200,
-          message: 'Sending',
-        })
+          message: "Sending",
+        });
       } catch (e) {
         return response.ok({
           code: 400,
-          message: 'User not found.',
+          message: "User not found.",
           exceptionMsg: e,
-        })
+        });
       }
     }
   }
 
-  public async resetPassword ({ request, response }: HttpContextContract) {
-    const reset = request.only(['email', 'password', 'token'])
+  public async resetPassword({ request, response }: HttpContextContract) {
+    const reset = request.only(["email", "password", "token"]);
 
     try {
-      const user = await User.findByOrFail('email', reset.email)
+      const user = await User.findByOrFail("email", reset.email);
       const token = await UseToken.findByOrFail(
-        'password_reset_token',
+        "password_reset_token",
         reset.token
-      )
+      );
 
-      const now = new Date()
+      const now = new Date();
       if (now > new Date(token.passwordResetExpires.toJSON())) {
-        return response.ok({
+        return response.badRequest({
           code: 400,
-          message: 'Token expired, try again',
-        })
+          message: "Token expired, try again",
+        });
       }
-      user.password = reset.password
-      return await user.save()
+      user.password = reset.password;
+      return await user.save();
     } catch (e) {
-      return response.ok({
+      return response.badRequest({
         code: 400,
-        message: 'Cannot reset password, try again',
+        message: "Cannot reset password, try again",
         exceptionMsg: e,
-      })
+      });
     }
   }
 }
--- e-books-back/app/Controllers/Http/UsersController.ts
+++ e-books-back/app/Controllers/Http/UsersController.ts
@@ -1,28 +1,28 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
-import Event from '@ioc:Adonis/Core/Event'
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
+import Event from "@ioc:Adonis/Core/Event";
 
-import UserValidator from 'App/Validators/UserValidator'
+import UserValidator from "App/Validators/UserValidator";
 
 export default class UsersController {
-  public async store ({ request, response }: HttpContextContract) {
-    const data = await request.validate(UserValidator)
-    let user = new User()
-    user.name = data.name
-    user.email = data.email
-    user.password = data.password
+  public async store({ request, response }: HttpContextContract) {
+    const data = await request.validate(UserValidator);
+    let user = new User();
+    user.name = data.name;
+    user.email = data.email;
+    user.password = data.password;
     try {
-      await user.save()
+      await user.save();
     } catch (e) {
       return response.badRequest({
         result: e,
         code: 500,
-        message: 'Insert fail',
-      })
+        message: `Insert fail - Duplicate entry ${user.email}`,
+      });
     }
 
-    Event.emit('user:send_mail', user.email)
+    Event.emit("user:send_mail", user.email);
 
-    return user
+    return user;
   }
 }
